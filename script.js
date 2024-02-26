033b30a60423dff6b2401b9b53640917ad1f2568
Veer <shoorveershah84@gmail.com>
2024-02-26 11:47:30 +0530

    initial commit added files

---
+++ script.js
@@ -0,0 +1,173 @@
+let startBtn = document.getElementById('start');
+let stopBtn = document.getElementById('stop');
+let resetBtn = document.getElementById('reset');
+
+let hour = 00;
+let minute = 00;
+let second = 00;
+let count = 00;
+
+startBtn.addEventListener('click', function(){
+    timer = true;
+    stopWatch();
+});
+
+stopBtn.addEventListener('click', function(){
+    timer = false;
+});
+
+resetBtn.addEventListener('click', function(){
+    timer = false;
+    hour = 0;
+    minute = 0;
+    second = 0;
+    count = 0;
+    document.getElementById('hr').innerHTML = "00";
+    document.getElementById('min').innerHTML = "00";
+    document.getElementById('sec').innerHTML = "00";
+    document.getElementById('count').innerHTML = "00";
+});
+
+function stopWatch(){
+    if (timer){
+        count++;
+
+        if (count == 100){
+            second++;
+            count =0;
+        }
+
+        if (second == 60){
+            minute++;
+            second=0;
+        }
+
+        if(minute == 60){
+            hour++;
+            minute=0;
+            second=0;
+        }
+
+        let hrString = hour;
+        let minString = minute;
+        let secString = second;
+        let countString = count;
+
+        if(hour <10){
+            hrString = "0" + hrString;
+        }
+
+        if (minute<10){
+            minString = "0" + minString;
+        }
+
+        if (second < 10){
+            secString = "0" + secString;
+        }
+
+        if (count < 10){
+            countString = "0" + countString;
+        }
+
+        document.getElementById('hr').innerHTML = hrString;
+        document.getElementById('min').innerHTML = minString;
+        document.getElementById('sec').innerHTML = secString;
+        document.getElementById('count').innerHTML = countString;
+        setTimeout(stopWatch, 10);
+    }
+}
+
+class Timer {
+    constructor(root) {
+      root.innerHTML = Timer.getHTML();
+  
+      this.el = {
+        minutes: root.querySelector(".timer__part--minutes"),
+        seconds: root.querySelector(".timer__part--seconds"),
+        control: root.querySelector(".timer__btn--control"),
+        reset: root.querySelector(".timer__btn--reset")
+      };
+  
+      this.interval = null;
+      this.remainingSeconds = 0;
+  
+      this.el.control.addEventListener("click", () => {
+        if (this.interval === null) {
+          this.start();
+        } else {
+          this.stop();
+        }
+      });
+  
+      this.el.reset.addEventListener("click", () => {
+        const inputMinutes = prompt("Enter number of minutes:");
+  
+        if (inputMinutes < 60) {
+          this.stop();
+          this.remainingSeconds = inputMinutes * 60;
+          this.updateInterfaceTime();
+        }
+      });
+    }
+  
+    updateInterfaceTime() {
+      const minutes = Math.floor(this.remainingSeconds / 60);
+      const seconds = this.remainingSeconds % 60;
+  
+      this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+      this.el.seconds.textContent = seconds.toString().padStart(2, "0");
+    }
+  
+    updateInterfaceControls() {
+      if (this.interval === null) {
+        this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
+        this.el.control.classList.add("timer__btn--start");
+        this.el.control.classList.remove("timer__btn--stop");
+      } else {
+        this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
+        this.el.control.classList.add("timer__btn--stop");
+        this.el.control.classList.remove("timer__btn--start");
+      }
+    }
+  
+    start() {
+      if (this.remainingSeconds === 0) return;
+  
+      this.interval = setInterval(() => {
+        this.remainingSeconds--;
+        this.updateInterfaceTime();
+  
+        if (this.remainingSeconds === 0) {
+          this.stop();
+        }
+      }, 1000);
+  
+      this.updateInterfaceControls();
+    }
+  
+    stop() {
+      clearInterval(this.interval);
+  
+      this.interval = null;
+  
+      this.updateInterfaceControls();
+    }
+  
+    static getHTML() {
+      return `
+              <span class="timer__part timer__part--minutes">00</span>
+              <span class="timer__part">:</span>
+              <span class="timer__part timer__part--seconds">00</span>
+              <button type="button" class="timer__btn timer__btn--control timer__btn--start">
+                  <span class="material-icons">play_arrow</span>
+              </button>
+              <button type="button" class="timer__btn timer__btn--reset">
+                  <span class="material-icons">timer</span>
+              </button>
+          `;
+    }
+  }
+  
+  new Timer(
+      document.querySelector(".timer")
+  );
